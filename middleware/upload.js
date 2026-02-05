1154552dd5399d33ff34aeed00b12ac971f397b8
nichoherjunaa <nichoherjunaa@gmail.com>
2026-02-05 14:49:22 +0700

    resize image for vercel blop

--- middleware/upload.js
+++ middleware/upload.js
@@ -6,7 +6,7 @@ const storage = multer.memoryStorage();
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 5 * 1024 * 1024
+        fileSize: 4 * 1024 * 1024
     },
     fileFilter: (req, file, cb) => {
         const allowedExtensions = ['.jpg', '.jpeg', '.png', '.webm'];
